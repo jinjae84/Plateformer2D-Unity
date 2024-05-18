4d73214e7661d70e9de94848384d651bef3a06d0
jinjae84 <166198670+jinjae84@users.noreply.github.com>
2024-05-18 18:00:42 +0900

    (2025.05.18) 함정 연속이동

---
+++ Plateformer Project/Assets/Scripts/Trap/Trap_Saw.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Tilemaps;
+using UnityEngine;
+
+public class Trap_Saw : Trap
+{
+    public Animator anim;
+    public Transform[] movePositions;        // 톱니바퀴가 이동할 변수
+    public float speed = 5f;
+    public int moveIndex = 0;
+    public bool onGoingFoward = true;
+    private SpriteRenderer spriteRenderer;
+    private void Start()
+    {
+
+
+        anim = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        isWorking = true;
+    }
+
+    private void Update() // 컴퓨터 성능 영향을 받는다.
+    {
+        anim.SetBool("isWorking", isWorking);
+
+        MoveTrap();
+    }
+
+    private void MoveTrap()
+    {
+        // 평균으로 0.0016 값
+        transform.position = Vector3.MoveTowards(transform.position, movePositions[moveIndex].position, speed * Time.deltaTime);
+        
+        // 조건문 - 함정이 목표한 지점까지 도착했는지?
+        if(Vector3.Distance(transform.position, movePositions[moveIndex].position) <= 0.1f)
+        {
+            if(moveIndex == 0)
+            {
+                Flip(onGoingFoward);
+                onGoingFoward = true;
+            }
+            if (onGoingFoward)
+                moveIndex++;
+            else
+                moveIndex--;
+
+            if(moveIndex >= movePositions.Length)
+            {
+                moveIndex = movePositions.Length - 1;
+                onGoingFoward = false;
+            }
+
+            
+        }
+        // 다음 목표 지점이 없으면.. move Index = 0.
+        
+
+    }
+
+    private void Flip(bool isFlip)
+    {
+        spriteRenderer.flipX = isFlip;
+    }
+
+    protected override void OnCollisionEnter2D(Collision2D collision)
+    {
+        base.OnCollisionEnter2D(collision);
+    }
+
+    protected override void OnTriggerEnter2D(Collider2D collision)
+    {
+        base.OnTriggerEnter2D(collision);
+    }
+}
